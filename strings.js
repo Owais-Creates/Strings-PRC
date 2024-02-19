d44f2fec56b16c5adca03ab8ff814073c0b12bf5
Owais <zaidalit00@gmail.com>
2024-02-19 19:30:28 +0530

    more solved questions

--- strings.js
+++ strings.js
@@ -1,20 +1,69 @@
-// let ques1str = "owais";
-// let revStr = "";
-// let splistr = ques1str.split("");
+// Q1 - Write a function that takes a string as input and returns the string reversed. For example, if the input is "hello", the output should be "olleh".
 
-// for (i = splistr.length; i <= 1; i++) {
-//     revStr += splistr[i];
+// let ques1String = prompt("Enter the String");
 
+// const reversedString = (str) => {
+//   let reversed = "";
+//   for ( i = ques1String.length-1 ; i<=0 ; i++ ){
+
+//     reversed += ques1String[i]
+
+//   }
+//   return reversed
 // }
 
-// console.log(revStr)
+// console.log(reversedString)
 
-let ques1str = "owais";
-let revStr = "";
-let splistr = ques1str.split("");
+// ---------------------------------------------------------------------------------------------------
 
-for (let i = splistr.length-1; i >= 0; i--) {
-    revStr += splistr[i];
+// Q2 - Write a function that takes a string as input and returns true if the string is a palindrome (reads the same forwards and backwards), and false otherwise. For example, "radar" is a palindrome, but "hello" is not.
+
+let ques2String = prompt("Enter the string");
+let ques2StringInput = ques2String.toLowerCase();
+
+const isPalindrome = (str) => {
+
+    let normalString = ""
+    let reversedString = ""
+
+    for (let i = 0; i < str.length; i++) {
+
+        normalString += str[i];
+        reversedString += str[str.length - 1 - i]
+    }
+
+    if (normalString == reversedString) {
+        console.log(`${str} is palindrome`)
+    } else {
+        console.log(`${str} is not palindrome`)
+    }
+
 }
 
-console.log(revStr);
+isPalindrome(ques2StringInput);
+
+// ---------------------------------------------------------------------------------------------------
+
+// Q3 - Write a function that takes a string as input and returns the number of vowels (a, e, i, o, u) in the string. For example, if the input is "hello", the output should be 2.
+
+let ques3String = prompt("Enter the String");
+let ques3StringInput = ques3String.toLowerCase();
+
+const numberOfVowels = (str) => {
+let count = 0;
+    
+for ( i = 0 ; i<str.length; i++ ){
+
+    if ( str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u" ) {
+
+        count ++ 
+    }
+}
+return count
+
+}
+
+console.log("Number of vowels" + " " + numberOfVowels(ques3StringInput));
+
+// ---------------------------------------------------------------------------------------------------
+
