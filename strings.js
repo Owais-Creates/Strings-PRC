075d7b7db03e1d8739740bd5a9010b9db1588e30
Owais <zaidalit00@gmail.com>
2024-02-19 20:42:05 +0530

    Implement functions for palindrome check, vowel count, and sentence capitalization

--- strings.js
+++ strings.js
@@ -18,52 +18,88 @@
 
 // Q2 - Write a function that takes a string as input and returns true if the string is a palindrome (reads the same forwards and backwards), and false otherwise. For example, "radar" is a palindrome, but "hello" is not.
 
-let ques2String = prompt("Enter the string");
-let ques2StringInput = ques2String.toLowerCase();
+// let ques2String = prompt("Enter the string");
+// let ques2StringInput = ques2String.toLowerCase();
 
-const isPalindrome = (str) => {
+// const isPalindrome = (str) => {
 
-    let normalString = ""
-    let reversedString = ""
+//     let normalString = ""
+//     let reversedString = ""
 
-    for (let i = 0; i < str.length; i++) {
+//     for (let i = 0; i < str.length; i++) {
 
-        normalString += str[i];
-        reversedString += str[str.length - 1 - i]
-    }
+//         normalString += str[i];
+//         reversedString += str[str.length - 1 - i]
+//     }
 
-    if (normalString == reversedString) {
-        console.log(`${str} is palindrome`)
-    } else {
-        console.log(`${str} is not palindrome`)
-    }
+//     if (normalString == reversedString) {
+//         console.log(`${str} is palindrome`)
+//     } else {
+//         console.log(`${str} is not palindrome`)
+//     }
 
-}
+// }
 
-isPalindrome(ques2StringInput);
+// isPalindrome(ques2StringInput);
 
 // ---------------------------------------------------------------------------------------------------
 
 // Q3 - Write a function that takes a string as input and returns the number of vowels (a, e, i, o, u) in the string. For example, if the input is "hello", the output should be 2.
 
-let ques3String = prompt("Enter the String");
-let ques3StringInput = ques3String.toLowerCase();
+// let ques3String = prompt("Enter the String");
+// let ques3StringInput = ques3String.toLowerCase();
 
-const numberOfVowels = (str) => {
-let count = 0;
-    
-for ( i = 0 ; i<str.length; i++ ){
+// const numberOfVowels = (str) => {
+// let count = 0;
 
-    if ( str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u" ) {
+// for ( i = 0 ; i<str.length; i++ ){
 
-        count ++ 
-    }
-}
-return count
+//     if ( str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u" ) {
 
-}
+//         count ++ 
+//     }
+// }
+// return count
 
-console.log("Number of vowels" + " " + numberOfVowels(ques3StringInput));
+// }
+
+// console.log("Number of vowels" + " " + numberOfVowels(ques3StringInput));
 
 // ---------------------------------------------------------------------------------------------------
+
+// Q-4 Write a function that takes a sentence (string) as input and returns the same sentence with the first letter of each word capitalized. For example, if the input is "hello world", the output should be "Hello World".
+
+let ques4String = prompt("Enter the string here");
+
+const upperCase = (str) => {
+    let finalResult = ""
+
+    finalResult += str[0].toUpperCase();
+
+    for (i = 1; i < str.length; i++) {
+
+        if (str[i] == " ") {
+
+            if (i + 1 < str.length) {
+
+                finalResult += ' ' + str[i + 1].toUpperCase();
+                i++;
+            }
+        }
+
+        else {
+            finalResult += str[i]
+        }
+
+    }
+
+
+    return finalResult
+
+
+}
+
+console.log(upperCase(ques4String));
+
+//---------------------------------------------------------------------------------------------------
 
