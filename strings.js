aec720455e3149ccb162249b4d2bd61b2d305d06
Owais <zaidalit00@gmail.com>
2024-02-20 19:11:16 +0530

    more solved questions

--- strings.js
+++ strings.js
@@ -103,6 +103,8 @@
 
 //---------------------------------------------------------------------------------------------------
 
+// Q5 - Write a function that takes two strings as input and returns true if they are anagrams of each other (contain the same characters in a different order), and false otherwise. For example, "listen" and "silent" are anagrams.
+
 let ques5String = prompt("Enter the first string");
 let ques5StringInput = ques5String.toLowerCase();
 
@@ -149,6 +151,32 @@ console.log(isAnagram(ques5StringInput, ques5StringTwoInput));
 
 //---------------------------------------------------------------------------------------------------
 
+// Q6 - Write a function that takes a string as input and returns the string with duplicate characters removed. For example, if the input is "hello", the output should be "helo".
 
+let ques6String = prompt("Enter the string");
+let ques6StringInput = ques6String.toLowerCase();
 
+const removeOccuringCharacter = (str) => {
+    let obj = {}
+    let result = "";
+
+    for (i = 0; i < str.length; i++) {
+        obj[str[i]] = (obj[str[i]] || 0) + 1;
+    }
+
+    for (let j = 0; j < str.length; j++) {
+        if (obj[str[j]] > 1) {
+            obj[str[j]] -= 1;
+        } else {
+            result += str[j];
+        }
+    }
+
+    return result
+
+}
+
+console.log(removeOccuringCharacter(ques6StringInput));
+
+//---------------------------------------------------------------------------------------------------
 
